c77db734b259e16966fbd0b3d4b346f7a52f40e6
GEROVVSKI <Admin@DESKTOP-AVHMASH>
2024-12-02 23:28:00 +0200

    Add project files.

---
+++ PiS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class PiS
+    {
+        public static double Perimetur(double a, double b, double c)
+        {
+            if (a+b>c&&a+c>b&&b+c>a)
+            {
+                return (a+b+c);
+            }
+            else
+            {
+                throw new ArgumentException("tova ne sa strani na triugulnik");
+            }
+        }
+
+
+        public static double Lice(double a, double b, double c)
+        {
+            double s = (a+b+c)/2;
+
+            return Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("vuvedi a:");
+            double a=double.Parse(Console.ReadLine());
+            Console.Write("vuvedi b:");
+            double b=double.Parse(Console.ReadLine());
+            Console.Write("vuvedi c:");
+            double c = double.Parse(Console.ReadLine());
+
+            double rezultat=PiS.Perimetur(a,b,c);
+            Console.Write($"perimeturut e:{rezultat}");
+            Console.WriteLine();
+            double rezultat1 = PiS.Lice(a, b, c);
+            Console.WriteLine($"liceto e:{rezultat1}");
+        }
+    }
+}
